43af973179ed6e8255d0af7ba341e9cc0bf548f1
Alexander Wolf <alexanderwolf@outlook.at>
2021-12-14 12:41:35 +0100

    fixed script bugs

--- install.sh
+++ install.sh
@@ -6,11 +6,33 @@ else
 	exit 1
 fi
 if [ -d "venv" ]; then
-	echo "Everything already installed, run run.sh to continue"
-	exit 1
+	echo "venv directory already exists, deleting and creating new one"
+	rm -rf ./venv
 fi
 python3 -m venv venv
 source venv/bin/activate
-cd Fahrplansystem
-pip3 install -r requirements.txt
-cd ..
+
+install_dependencies () {
+	echo "Trying to install dependecies for $1"
+	if [ -d "$1" ]; then
+		echo "$1 directory found"
+		cd "$1"
+	else
+		echo "$1 directory could not be found, no dependencies will be installed"
+		return
+	fi
+	if [ -f "requirements.txt" ]; then
+ 		echo "Requirements file for $1 found"
+ 		pip3 install -r requirements.txt &> /dev/null
+		echo "All requirements for $1 successfully installed"
+		cd ..
+	else
+ 		echo "Requirements file for $1 not found, no dependencies will be installed"
+		cd ..
+		return
+	fi
+}
+
+install_dependencies "Fahrplansystem"
+install_dependencies "Flottensystem"
+install_dependencies "Streckensystem"
--- run.sh
+++ run.sh
@@ -1,6 +1,38 @@
+#!/bin/bash
+trap "exit" INT TERM
 trap "kill 0" EXIT
+if [ ! -d "venv" ]; then
+	echo "venv directory not found, run install.sh first"
+    exit 1
+fi
 source venv/bin/activate
-cd Fahrplansystem
-flask run -p 5001 >/dev/null 2>&1 & 
-echo "Fahrplansystem running on 127.0.0.1:5001"
+
+run_server () {
+    if [ -d "$1" ]; then
+	    echo "$1 directory found"
+        cd "$1"
+    else
+        echo "$1 directory not found, not starting server"
+        return
+    fi
+    if [[ $(lsof -i -P -n | grep "$2" | grep -i "Python") ]]; then
+        echo "Flask application $1 on port $2 is already running"
+        cd ..
+        return
+    fi
+    if [ -f ".flaskenv" ]; then
+	    echo ".flaskenv file for $1 system found"
+        flask run -p $2 &> /dev/null &
+        echo "Running $1 on localhost:$2"
+        cd ..
+    else
+        echo ".flaskenv file not found, not starting server"
+        cd ..
+        return
+    fi
+}
+
+run_server "Fahrplansystem" 5000
+run_server "Flottensystem" 5001
+run_server "Streckensystem" 5002
 wait
